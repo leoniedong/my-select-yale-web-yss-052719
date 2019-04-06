ddc8170ead8e74f2362af6f716722b877d23f25c
Leonie Dong <leoniedong@hotmail.com>
2019-04-06 03:08:53 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -2,7 +2,7 @@ def my_select(collection)
  i = 0
  result = []
  while i < collection.length
-   if collection[i].gets.chomp
+   if collection[i]. == true
      result << collection[i]
    end
  end
